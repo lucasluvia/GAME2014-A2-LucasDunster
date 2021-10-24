2daab43b7aa8e633327ca8b1fa7d89ad4216f7fe
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2021-10-23 22:35:00 -0400

    1.1.02 - Player Movement
    Added Player movement through UI button presses and bounds to keep the player on screen

--- Assets/[Scripts]/ButtonBehaviour.cs
+++ Assets/[Scripts]/ButtonBehaviour.cs
@@ -7,10 +7,29 @@ public class ButtonBehaviour : MonoBehaviour
 {
     [SerializeField]
     private int buttonDestination = 0;
+    [SerializeField]
+    private Rigidbody2D playerRB = null;
 
 
     public void OnButtonPressed()
     {
         SceneManager.LoadScene(buttonDestination);
     }
+    public void OnLeftPressed()
+    {
+        MoveCharacter(-1.0f);
+    }
+
+    public void OnRightPressed()
+    {
+        MoveCharacter(1.0f);
+    }
+
+    private void MoveCharacter(float direction)
+    {
+        Vector2 newVelocity = playerRB.velocity + new Vector2(direction * 2.0f, 0.0f);
+        playerRB.velocity = Vector2.ClampMagnitude(newVelocity, 6.0f);
+        playerRB.velocity *= 0.99f;
+    }
+
 }
--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private float screenBound;
+
+    private Rigidbody2D body;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckBounds();
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.x >= screenBound)
+        {
+            transform.position = new Vector3(screenBound - 0.05f, transform.position.y, 0.0f);
+            body.velocity = new Vector2(0.0f, 0.0f);
+        }
+
+        if (transform.position.x <= -screenBound)
+        {
+            transform.position = new Vector3(-screenBound + 0.05f, transform.position.y, 0.0f);
+            body.velocity = new Vector2(0.0f,0.0f);
+        }
     }
 }
